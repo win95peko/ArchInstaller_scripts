edfa4061274807689e0f48446caa3db21553c783
win95peko <32701718+win95peko@users.noreply.github.com>
2022-05-12 16:33:11 -0300

    Update pacstrap.sh
    replaced package network-manager with networkmanager in the command pacstarp

--- pacstrap.sh
+++ pacstrap.sh
@@ -2,4 +2,4 @@
 pacman -Sy
 mount /dev/sda1 /mnt
 
-pacstrap /mnt base base-devel linux linux-firmware git alacritty qtile polybar xorg xorg-server xorg-apps vlc firefox xorg-xrdb adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts otf-ipafont network-manager pulseaudio-alsa pulseaudio efibootmgr grub lightdm lightdm-slick-greeter ttf-croscore gnu-free-fonts ttf-liberation noto-fonts ttf-jetbrains-mono cantarell-fonts ttf-fira-sans otf-crimson ttf-opensans ttf-bitstream-vera
+pacstrap /mnt base base-devel linux linux-firmware git alacritty qtile polybar xorg xorg-server xorg-apps vlc firefox xorg-xrdb adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts otf-ipafont networkmanager pulseaudio-alsa pulseaudio efibootmgr grub lightdm lightdm-slick-greeter ttf-croscore gnu-free-fonts ttf-liberation noto-fonts ttf-jetbrains-mono cantarell-fonts ttf-fira-sans otf-crimson ttf-opensans ttf-bitstream-vera
